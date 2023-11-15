3bd5127cc0ad37cc725874fb3a0f6d1e25fefb2b
Sebastian Allende <sallende@miuandes.cl>
2023-11-15 20:13:13 -0300

    feat: register screen

--- src/components/Sesion/Register.jsx
+++ src/components/Sesion/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { RegisterUser } from '../../api';
 import { useNavigate } from 'react-router-dom';
 import "./Register.css";
 
@@ -7,46 +7,57 @@ const Register = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
-    const handleRegister = async () => {
-        try {
-            const response = await axios.post('http://localhost:8000/api/login/', {
-                username: username,
-                password: password,
-            });
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-            console.log(response.data);  
-        
-        } catch (error) {
-            console.error('Error al iniciar sesión', error);
-            
-        }
+    const handleResponse = (resp) => {
+        console.log(resp)
+    }
+
+    const handleRegister = () => {
+        RegisterUser({ username: username, email: email, password: password }, handleResponse)
     };
 
     return (
-        <div>
+        <>
             <div className='login-back'>
                 <div className="go-back-login" onClick={() => navigate("/login")}>
                     &#60;
                 </div>
                 <div className="login-name">
-                    Iniciar Sesion
+                    Volver a inicio de sesion
                 </div>
             </div>
             <div className='logo-sesion'>
                 <img className='icono-sesion' src='/img/hamburguer.png' alt='Search' />
                 <h1 className='qcomparator-sesion'>QComparator</h1>
             </div>
-            <div>
-                <label>Username:</label>
+            <div className='user-input'>
+                <label>Nombre de usuario:</label>
                 <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                
-                <button onClick={handleRegister}>Register</button>
+
+                <label>Mail:</label>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+                <label>Contraseña:</label>
+                <div className='password-container'>
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <span className="toggle-password" onClick={togglePasswordVisibility}>
+                        {showPassword ? '👁️' : '🙈'}
+                    </span>
+                </div>
+
+                <button onClick={handleRegister}>Registrar cuenta!</button>
             </div>
-        </div>
+        </>
     );
 };
 
