60c1c464cebda04bbbfb51c9d09a6ef20c0c4f28
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-11-15 23:53:19 -0300

    new review

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+// PrivateRoute.jsx
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element, authenticated, ...props }) => {
+    return authenticated ? (
+        <Route {...props} element={element} />
+    ) : (
+        <Navigate to="/login" />
+    );
+};
+
+export default PrivateRoute;
--- src/components/Product/NewReview.jsx
+++ src/components/Product/NewReview.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { ReviewCreate, GetProduct } from '../../api';
 import { useGlobalContext } from '../../context';
+import './NewReview.css';
 
 const NewReview = () => {
   const { id } = useParams();
@@ -12,6 +13,7 @@ const NewReview = () => {
 
   const [rating, setRating] = useState('');
   const [description, setDescription] = useState('');
+  const [displayError, setDisplayError] = useState(null);
 
   const handleRatingChange = (event) => {
     setRating(event.target.value);
@@ -23,49 +25,61 @@ const NewReview = () => {
 
   const handleSubmit = () => {
     ReviewCreate({ user: userid, producto: id, comentario: description, calificacion: parseInt(rating, 10) }, handleResponse);
-    navigate(`/product/${id}/reviews`);
   };
 
   const handleResponse = (resp) => {
-    if (resp.message != "Invalid credentials") {
+    if (resp.message !== "Invalid credentials") {
         navigate(`/product/${id}/reviews`);
     } else {
         alert('Clasificación o descripcion incorrectas .');
         setDisplayError(resp.errors);
     }
+  };
 
-    };
-
-    useEffect(() => {
-        if (!name) {
-            GetProduct(id, handleData);
-        }
-    })
-
-    const handleData = (data) => {
-        setName(data.nombre);
+  useEffect(() => {
+    if (!name) {
+      GetProduct(id, handleData);
     }
+  });
+
+  const handleData = (data) => {
+    setName(data.nombre);
+  };
 
   return (
-    <div>
-      <div className='name-back'>
-        <div className='go-back' onClick={() => navigate(`/product/${id}/reviews/`)}>
-          &#60;
+    <div className='new-review-container'>
+      <div className='new-review-header'>
+        <div className='new-review-go-back' onClick={() => navigate(`/product/${id}/reviews/`)}>
+          &#60; Volver
         </div>
-        <div className='product-name'>{name}</div>
+        <div className='new-review-product-name'>{name}</div>
       </div>
 
-      <div>
-        <label>Calificación (1-5):</label>
-        <input type='number' min='1' max='5' value={rating} onChange={handleRatingChange} />
+      <div className='new-review-input-container'>
+        <label className='new-review-input-label'>Calificación (1-5):</label>
+        <div className='new-review-rating-container'>
+          {[1, 2, 3, 4, 5].map((value) => (
+            <div
+              key={value}
+              className={`new-review-rating-star ${rating >= value ? 'active' : ''}`}
+              onClick={() => setRating(value)}
+            >
+              {value}
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div>
-        <label>Descripción:</label>
-        <textarea value={description} onChange={handleDescriptionChange} />
+      <div className='new-review-input-container'>
+        <label className='new-review-input-label'>Descripción:</label>
+        <textarea className='new-review-input' value={description} onChange={handleDescriptionChange} />
       </div>
 
-      <button onClick={handleSubmit}>Guardar Review</button>
+      {displayError && <div className='new-review-error-message'>{displayError}</div>}
+
+      <button className='new-review-submit-button' onClick={handleSubmit}>
+        Guardar Review
+      </button>
     </div>
   );
 };
